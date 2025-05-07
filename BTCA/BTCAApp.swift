e15f7188636277f2aa1254c775839cedb27b6699
OldAndStillLearning <amalia.buzz@gmail.com>
2025-05-06 20:53:14 -0400

    Initial Commit

---
+++ BTCA/BTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCAApp.swift
+//  BTCA
+//
+//  Created by call151 on 2025-05-06.
+//
+
+import SwiftUI
+
+@main
+struct BTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
